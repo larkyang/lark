ed1e8b1f46baf2cc3808dcb9de1bf3ec93eb0a49
aaa <aaa@aaa>
2022-10-18 20:32:42 +0800

    加入按鈕，用Toast顯示

--- app/src/main/java/com/example/spinnerapp/MainActivity.kt
+++ app/src/main/java/com/example/spinnerapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.spinnerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spGender = findViewById<Spinner>(R.id.spGender)
+        val btOk = findViewById<Button>(R.id.btOk)
+        btOk.setOnClickListener {
+            Toast
+                .makeText(this,"${spGender.selectedItem.toString()}", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }
